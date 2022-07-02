07b27527f2825dfb2615eb15efd5aae000e8a4cc
Roy Sigurd Karlsbakk <roysk@hioa.no>
2022-07-02 15:01:43 +0200

    ny konfig som ikke trengs

--- Marlin/CR10-S/config/2022-07-02/vaske-konfig-fra-m503.pl
+++ Marlin/CR10-S/config/2022-07-02/vaske-konfig-fra-m503.pl
@@ -1,4 +1,5 @@
 #!/usr/bin/perl
+# vim:ts=4:sw=4:sts=4:et:ai:fdm=marker
 #
 # Les dump fra M503 og skriv om til tolkbar gkode
 #
@@ -6,4 +7,16 @@
 use strict;
 use warnings;
 
-# Se dump.txt
+# Utsnitt fra dump.txt {{{
+#
+# Recv: echo:; Filament settings: Disabled
+# Recv: echo:  M200 S0 D1.75
+# Recv: echo:; Steps per unit:
+# Recv: echo: M92 X80.00 Y80.00 Z400.00 E728.00
+#
+# }}}
+while (<STDIN>) {
+    next unless (/^Recv: echo:/);
+    s/Recv: echo:\s*//;
+    print;
+}
